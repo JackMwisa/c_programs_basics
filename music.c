8eca3ae65c4ea9a70b00f526b0b12d0ed45cea9e
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-09-04 09:52:33 +0300

    Update music.c

--- music.c
+++ music.c
@@ -13,6 +13,7 @@ typedef struct Playlist {
     Song* head;
     Song* tail;
     Song* current;
+    
 } Playlist;
 
 // Function to create a new song node
@@ -106,14 +107,12 @@ void nextSong(Playlist* playlist) {
     }
 }
 
-
 // Function to navigate to the previous song
 void previousSong(Playlist* playlist) {
     if (playlist->current == playlist->head) {
         printf("You are at the start of the playlist.\n");
         return;
     }
-
     
     Song* temp = playlist->head;
     while (temp != NULL && temp->next != playlist->current) {
